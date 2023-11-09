a3186920d0f94e2a357ffbbbe9088fc0ff5e80ba
itspariya <149511635+itspariya@users.noreply.github.com>
2023-11-08 19:53:20 -0500

    Update utility.h

--- iptools/utility/utility.h
+++ iptools/utility/utility.h
@@ -3,12 +3,11 @@
 
 #include "../image/image.h"
 #include <opencv2/opencv.hpp>
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/core/core.hpp"
+//#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
 #include <sstream>
 #include <math.h>
-#include <string>
 
 class utility
 {
@@ -22,14 +21,15 @@ class utility
 		static void scale(image &src, image &tgt, float ratio);
 		static void cv_gray(cv::Mat &src, cv::Mat &tgt);
 		static void cv_avgblur(cv::Mat &src, cv::Mat &tgt, int WindowSize);
-		static void histogramStretching(image &src, image &tgt, int A, int B);
-		static void histogramStretch(cv::Mat& src,cv::Mat &tgt, int A, int B);
-		static void histogramEqualization(cv::Mat &src, cv::Mat &tgt);
-		static void equalizeDarkPixels(cv::Mat &src, cv::Mat &tgt, int threshold);
-		static void greyAugmentedImages(cv::Mat &src, cv::Rect &ROI_rect, int A, int B);
-		static void equalizeRGB(cv::Mat& src, cv::Mat& tgt, const std::string& channels);
-		static void equalizeHSI(cv::Mat& src, cv::Mat& tgt, bool equalizeH, bool equalizeS, bool equalizeI);
-		static void colorAugmentedImages(cv::Mat &src, cv::Rect &ROI_rect);
+		static void applyFunctionsToROIs(image &src, image &tgt, 
+										const std::vector<cv::Rect>& rois, 
+										const std::vector<std::string>& functions, 
+										const std::vector<std::vector<int>>& params);
+		static void applyCvFunctionsToROIs(cv::Mat &src, cv::Mat &tgt, 
+                                       const std::vector<cv::Rect>& rois, 
+                                       const std::vector<std::string>& functions, 
+                                       const std::vector<std::vector<int>>& params);
 };
 
 #endif
+
