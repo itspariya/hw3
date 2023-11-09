b80644b0334a2a6001ed143633db4de7570cad5d
itspariya <149511635+itspariya@users.noreply.github.com>
2023-11-08 19:52:33 -0500

    Update iptool.cpp

--- project/iptool.cpp
+++ project/iptool.cpp
@@ -1,149 +1,113 @@
 #include "../iptools/core.h"
-#include <string.h>
+#include "../iptools/utility/utility.h"
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
 
 using namespace std;
+using namespace cv;
 
 #define MAXLEN 256
 
-int main (int argc, char** argv)
-{
-	image src, tgt;
-	FILE *fp;
-	char str[MAXLEN];
-	char infile[MAXLEN];
-	char outfile[MAXLEN];
-	char *pch;
-	
-	if ((fp = fopen(argv[1],"r")) == NULL) {
-		fprintf(stderr, "Can't open file: %s\n", argv[1]);
-		exit(1);
-	}
+int main(int argc, char** argv) {
+    FILE *fp;
+    char str[MAXLEN];
+    char infile[MAXLEN];
+    char outfile[MAXLEN];
+    char *pch;
+    
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Can't open file: %s\n", argv[1]);
+        exit(1);
+    }
 
-	while(fgets(str,MAXLEN,fp) != NULL) {
-		if (strncmp(str,"#",1)==0) continue;
-		int NumParameters = 0;
-		
-		pch = strtok(str, " ");
-		strcpy(infile, pch);
+    while (fgets(str, MAXLEN, fp) != NULL) {
+        if (strncmp(str, "#", 1) == 0) continue;
 
-		pch = strtok(NULL, " ");
-		strcpy(outfile, pch);
+        pch = strtok(str, " ");
+        strcpy(infile, pch);
 
-		pch = strtok(NULL, " ");
-		if (strncmp(pch,"opencv",6)==0) {
-			cv::Mat I = cv::imread(infile);
-			cv::Mat I2;
-			
-			if( I.empty()) {
-				cout << "Could not open or find the image: " << infile << endl;
-				return -1;
-			}
+        pch = strtok(NULL, " ");
+        strcpy(outfile, pch);
 
-			//cv::namedWindow("Grayscale Image", cv::WINDOW_AUTOSIZE);
-			
-			pch = strtok(NULL, " ");
-			if (strncmp(pch,"gray",4)==0) {
-				utility::cv_gray(I, I2);
-			}
-			else if (strncmp(pch,"blur_avg",8)==0) {
-				pch = strtok(NULL, " ");
-				utility::cv_avgblur(I, I2, atoi(pch));
-			}
-			else if (strncmp(pch,"equalize",8)==0) {
-				
-    			utility::histogramEqualization(I, I2);
-			}
+        pch = strtok(NULL, " ");
+        bool useOpenCV = strncmp(pch, "opencv", 6) == 0;
 
-			else if (strncmp(pch,"equalize_dark",13)==0) {
-				pch = strtok(NULL, " ");
-				int threshold = atoi(pch);
-				utility::equalizeDarkPixels(I, I2, threshold);
-			}
-			
-			else if (strncmp(pch,"stretch",17)==0) {
-				pch = strtok(NULL, " ");
-				int A = atoi(pch);
-				pch = strtok(NULL, " ");
-				int B = atoi(pch);
-				utility::histogramStretch(I, I2, A, B);
-			}
-			else if (strncmp(pch, "grey_augment", 12) == 0) {
-				pch = strtok(NULL, " ");
-				int x = atoi(pch);
-				pch = strtok(NULL, " ");
-				int y = atoi(pch);
-				pch = strtok(NULL, " ");
-				int width = atoi(pch);
-				pch = strtok(NULL, " ");
-				int height = atoi(pch);
-				pch = strtok(NULL, " ");
-				int A = atoi(pch);
-				pch = strtok(NULL, " ");
-				int B = atoi(pch);
-				
-				cv::Rect ROI_rect(x, y, width, height);
-				utility::greyAugmentedImages(I, ROI_rect, A, B);
-			}
-			else if (strncmp(pch, "equalize_rgb", 12) == 0) {
-				pch = strtok(NULL, " ");
-				std::string channels(pch);
-				utility::equalizeRGB(I, I2, channels);
-			}
-			else if (strncmp(pch, "equalize_hsi", 12) == 0) {
-				pch = strtok(NULL, " ");
-				bool equalizeH = atoi(pch);
-				pch = strtok(NULL, " ");
-				bool equalizeS = atoi(pch);
-				pch = strtok(NULL, " ");
-				bool equalizeI = atoi(pch);
-				utility::equalizeHSI(I, I2, equalizeH, equalizeS, equalizeI);
-			}
-			else if (strncmp(pch, "color_augment", 12) == 0) {
-				pch = strtok(NULL, " ");
-				int x = atoi(pch);
-				pch = strtok(NULL, " ");
-				int y = atoi(pch);
-				pch = strtok(NULL, " ");
-				int width = atoi(pch);
-				pch = strtok(NULL, " ");
-				int height = atoi(pch);
-				
-				cv::Rect ROI_rect(x, y, width, height);
-				utility::colorAugmentedImages(I, ROI_rect);
-			}
-			
-			else {
-				printf("No function: %s\n", pch);
-				continue;
-			}
-			
-			cv::imwrite(outfile, I2);
-		}
-		else {
-			src.read(infile);
-			if (strncmp(pch,"add",3)==0) {
-				pch = strtok(NULL, " ");
-				utility::addGrey(src,tgt,atoi(pch));
-			}
+        Mat I, I2;
+        image src, tgt;
+        if (useOpenCV) {
+            I = imread(infile);
+            if (I.empty()) {
+                cout << "Could not open or find the image: " << infile << endl;
+                return -1;
+            }
+            I2 = I.clone(); 
+        } else {
+            src.read(infile);
+            tgt = src; 
+        }
 
-			else if (strncmp(pch,"binarize",8)==0) {
-				pch = strtok(NULL, " ");
-				utility::binarize(src,tgt,atoi(pch));
-			}
+        // Apply global functions if specified
+        pch = strtok(NULL, " ");
+        if (useOpenCV) {
+            // Apply OpenCV global functions
+            if (pch != NULL && strncmp(pch, "gray", 4) == 0) {
+                utility::cv_gray(I, I2);
+            } else if (pch != NULL && strncmp(pch, "blur_avg", 8) == 0) {
+                int windowSize = atoi(strtok(NULL, " "));
+                utility::cv_avgblur(I, I2, windowSize);
+            }
+        } else {
+            // Apply non-OpenCV global functions
+            if (pch != NULL && strncmp(pch, "add", 3) == 0) {
+                pch = strtok(NULL, " ");
+                utility::addGrey(src, tgt, atoi(pch));
+            } else if (pch != NULL && strncmp(pch, "binarize", 8) == 0) {
+                pch = strtok(NULL, " ");
+                utility::binarize(src, tgt, atoi(pch));
+            } else if (pch != NULL && strncmp(pch, "scale", 5) == 0) {
+                pch = strtok(NULL, " ");
+                utility::scale(src, tgt, atof(pch));
+            }
+        }
 
-			else if (strncmp(pch,"scale",5)==0) {
-				pch = strtok(NULL, " ");
-				utility::scale(src,tgt,atof(pch));
-			}
+        // Parse the number of ROIs
+        pch = strtok(NULL, " ");
+        int numROIs = atoi(pch);
+        vector<Rect> rois;
+        vector<string> functions;
+        vector<vector<int>> params;
 
-			else {
-				printf("No function: %s\n", pch);
-				continue;
-			}
-			
-			tgt.save(outfile);
-		}
-	}
-	fclose(fp);
-	return 0;
+        for (int i = 0; i < numROIs; ++i) {
+            // Parse ROI parameters
+            int x = atoi(strtok(NULL, " "));
+            int y = atoi(strtok(NULL, " "));
+            int width = atoi(strtok(NULL, " "));
+            int height = atoi(strtok(NULL, " "));
+            rois.push_back(Rect(x, y, width, height));
+
+            // Parse the function name
+            pch = strtok(NULL, " ");
+            functions.push_back(string(pch));
+
+            // Parse the function parameters
+            vector<int> funcParams;
+            while ((pch = strtok(NULL, " ")) && isdigit(pch[0])) {
+                funcParams.push_back(atoi(pch));
+            }
+            params.push_back(funcParams);
+        }
+
+        // Apply ROI-specific functions
+        if (useOpenCV) {
+            utility::applyCvFunctionsToROIs(I2, I2, rois, functions, params);
+            imwrite(outfile, I2);
+        } else {
+            utility::applyFunctionsToROIs(src, tgt, rois, functions, params);
+            tgt.save(outfile);
+        }
+    }
+
+    fclose(fp);
+    return 0;
 }
